05a4910986877d294b3eeac7ee3d2a633bc89b87
Julie Allinson <julie.allinson@london.ac.uk>
2019-05-02 15:36:30 +0000

    create a static public ip

---
+++ main.tf
@@ -0,0 +1,36 @@
+provider "azurerm" {
+  subscription_id = "${var.subscription_id}"
+  client_id = "${var.client_id}"
+  client_secret = "${var.client_secret}"
+  tenant_id = "${var.tenant_id}"
+}
+
+# Labels
+module "labels" {
+  source = "devops-workflow/label/local"
+  version = "0.2.1"
+
+  # Required
+  environment = "${var.environment}"
+  name = "${var.name}"
+  # Optional
+  namespace-org = "${var.namespace-org}"
+  organization = "${var.org}"
+  delimiter = "-"
+  owner = "${var.owner}"
+  team = "${var.team}"
+  tags {
+    Name = "${module.labels.id}"
+  }
+}
+
+# Azure Public IP
+resource "azurerm_public_ip" "default" {
+  name = "${module.labels.id}"
+  location = "${var.location}"
+  resource_group_name = "${module.labels.id}"
+  resource_group_name = "MC_${module.labels.id}_${module.labels.id}_${var.location}"
+  allocation_method   = "Static"
+
+  tags = "${module.labels.tags}"
+}
---
+++ variables.tf
@@ -0,0 +1,18 @@
+variable "subscription_id" {
+    description = "Azure subscription ID"
+}
+variable "client_id" {
+  description = "Azure Client ID"
+}
+
+variable "client_secret" {
+  description = "Azure Client Secret"
+}
+variable "tenant_id" {
+    description = "Azure tenant ID."
+}
+
+variable "location" {
+  description = "Location"
+  default = "northeurope"
+}

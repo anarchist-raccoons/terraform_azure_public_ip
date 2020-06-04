ce8f0d9be1d14def6538c9cd9a34b4ad0fd2101a
Rory McNicholl <rory.mcnicholl@london.ac.uk>
2020-06-04 14:10:56 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -20,9 +20,9 @@ module "labels" {
   delimiter = "-"
   owner = "${var.owner}"
   team = "${var.team}"
-  tags {
-    Name = "${module.labels.id}"
-  }
+#  tags {
+#    Name = "${module.labels.id}"
+#  }
 }
 
 # Azure Public IP

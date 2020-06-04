7d7f9ca4fec294221b9c614466d1759ee0e63081
Rory McNicholl <rory.mcnicholl@london.ac.uk>
2020-06-04 14:05:51 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -29,7 +29,7 @@ module "labels" {
 resource "azurerm_public_ip" "default" {
   name = "${var.service_name}"
   location = "${var.location}"
-  resource_group_name = "${module.labels.id}"
+  #resource_group_name = "${module.labels.id}"
   resource_group_name = "${var.resource_group}"
   allocation_method   = "Static"
 

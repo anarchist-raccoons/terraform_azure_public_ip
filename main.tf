40286f5b6f92b2239b46b1a947d22d662cd3021c
Rory McNicholl <rory.mcnicholl@london.ac.uk>
2021-06-15 09:25:00 +0000

    Quoted references are deprecated

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 provider "azurerm" {
-  subscription_id = "${var.subscription_id}"
-  client_id = "${var.client_id}"
-  client_secret = "${var.client_secret}"
-  tenant_id = "${var.tenant_id}"
+  subscription_id = var.subscription_id
+  client_id = var.client_id
+  client_secret = var.client_secret
+  tenant_id = var.tenant_id
   features {}
 }
 
@@ -20,26 +20,26 @@ module "labels" {
   tags = { Name = module.labels.id }
 
 #  # Required
-#  environment = "${var.environment}"
-#  name = "${var.name}"
+#  environment = var.environment
+#  name = var.name
 #  # Optional
-#  namespace-org = "${var.namespace-org}"
-#  organization = "${var.org}"
+#  namespace-org = var.namespace-org
+#  organization = var.org
 #  delimiter = "-"
-#  owner = "${var.owner}"
-#  team = "${var.team}"
+#  owner = var.owner
+#  team = var.team
 #  tags = {
-#    Name = "${module.labels.id}"
+#    Name = module.labels.id
 #  }
 }
 
 # Azure Public IP
 resource "azurerm_public_ip" "default" {
-  name = "${var.service_name}"
-  location = "${var.location}"
-  #resource_group_name = "${module.labels.id}"
-  resource_group_name = "${var.resource_group}"
+  name = var.service_name
+  location = var.location
+  #resource_group_name = module.labels.id
+  resource_group_name = var.resource_group
   allocation_method   = "Static"
 
-  tags = "${module.labels.tags}"
+  tags = module.labels.tags
 }
